7f96993719353a0ecb86ea66b1ce63a1b428eece
Johansson <axel.johansson@if.fi>
2016-07-01 15:53:06 +0300

    toimii, mutta tekstin koodaus on väärä

--- elementparser.py
+++ elementparser.py
@@ -2,12 +2,23 @@
 
 import argparse
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as DOM
 import json
+
+outputfile = open("output.xml", 'a')
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = DOM.parseString(rough_string)
+    reparsed.writexml(outputfile, indent="	")
 
 parser = argparse.ArgumentParser(description='XML parser')
 parser.add_argument('inputf', metavar='<Input File>', type=str, help='input file')
 parser.add_argument('pairs', type=str, nargs="*", help='Define key-value pairs. Syntax: key1:value1 key2:value2')
 parser.add_argument('--path', type=str, help='Define path to root element where program starts searching for child elements. Syntax: parent/child/grandchild. If not defined, document root used as root element.')
+parser.add_argument('--output', type=str, help='Output file')
 
 # parse args
 args = parser.parse_args()
@@ -23,10 +34,12 @@ for pairstr in pairList:
 			break
 		i += 1
 
+print("<!-- Search parameters")
 for key in pairs:
 	print("key: ",key)
 	print("value: ", pairs[key])
 	print()
+print("-->")
 
 # define nodes
 if args.path is not None:
@@ -44,9 +57,15 @@ else:
 	roots = []
 	roots.append(root)
 
-for r in roots:
-	for child in r:
-		for key in pairs:
-			if child.tag == key and child.text == pairs[key]:
-				ET.dump(r)
+for r in roots: #jokainen juuri käydään läpi
+	for key in pairs: #jokainen avain käydään läpi
+		found = False #oletus: ei löydy
+		for child in r: #jokainen lapsi käydään läpi
+			if child.tag == key and child.text == pairs[key]: #oikea lapsi löytyi -> found = true
+				found = True
 				break
+		if not found: #kaikki lapset käytiin läpi löytämättä -> seuraava juuri
+			break
+	if found:
+		prettify(r)
+		print("", file=outputfile)
