46747b0ee357e804521e03580c2e4dde1c46c6bd
Johansson <axel.johansson@if.fi>
2016-07-04 13:19:14 +0300

    Iteroidaan läpi kaikki polut ja poistetaan turhat

--- elementparser.py
+++ elementparser.py
@@ -3,17 +3,9 @@
 import argparse
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as DOM
-import json
+from time import sleep
 
-outputfile = open("output.xml", 'a')
-
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
-    rough_string = ET.tostring(elem, 'utf-8')
-    reparsed = DOM.parseString(rough_string)
-    reparsed.writexml(outputfile, indent="	")
-
+# init parser
 parser = argparse.ArgumentParser(description='XML parser')
 parser.add_argument('inputf', metavar='<Input File>', type=str, help='input file')
 parser.add_argument('pairs', type=str, nargs="*", help='Define key-value pairs. Syntax: key1:value1 key2:value2')
@@ -34,38 +26,68 @@ for pairstr in pairList:
 			break
 		i += 1
 
-print("<!-- Search parameters")
+print("Search parameters")
 for key in pairs:
 	print("key: ",key)
 	print("value: ", pairs[key])
 	print()
-print("-->")
 
 # define nodes
 if args.path is not None:
-	path = "./" + args.path
+	i=0
+	j=0
+	k=0
+	path=[]
+	for char in args.path:
+		j+=1
+		if char == '/':
+			path.append(args.path[k:j-1])
+			k=j
+	path.append(args.path[k:])
+	print("Path: ", path)
 
 # parse tree from xml
 tree = ET.parse(args.inputf)
-root = tree.getroot()
-dictList = []
-elementList = []
+roots = []
+roots.append(tree.getroot())
 
-if args.path is not None:
-	roots = root.findall(path)
-else:
-	roots = []
-	roots.append(root)
+# loop through nodes and remove unwanted elements
+for i, node in enumerate(path):
+	print("iteration: ", i+1, " node: ", node)
+	newroots = []
+	for root in roots:
+		#print("root: ", root.tag)
+		for child in list(root):
+			#print("child ", child.tag)
+			if child.tag != node:
+				print("remove ", child.tag)
+				root.remove(child)
+			else:
+				if i == len(path) -1: # last node
+					# print("last node")
+					found = True
+					for key, value in pairs.items():
+						if not found:
+							break
+						found = False
+						# print(key, value)
+						#sleep(0.5)
+						for c in child:
+							if c.tag == key:
+								if c.text == value:
+									print("Found ", c.tag, " ", c.text)
+									found = True
+									break
+					if not found:
+						print("remove ", child.tag)
+						root.remove(child)
+					else: print("keep ", child.tag)
+				else:
+					print("keep ", child.tag) 
+					newroots.append(child)
+	roots = newroots
 
-for r in roots: #jokainen juuri käydään läpi
-	for key in pairs: #jokainen avain käydään läpi
-		found = False #oletus: ei löydy
-		for child in r: #jokainen lapsi käydään läpi
-			if child.tag == key and child.text == pairs[key]: #oikea lapsi löytyi -> found = true
-				found = True
-				break
-		if not found: #kaikki lapset käytiin läpi löytämättä -> seuraava juuri
-			break
-	if found:
-		prettify(r)
-		print("", file=outputfile)
+# write to output
+if args.output is None:
+	args.output = 'output.xml'
+tree.write(args.output, encoding="UTF-8", xml_declaration=True)
